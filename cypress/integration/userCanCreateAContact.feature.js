af8c2801b7508bd8589a72b587b2af5a60f2b0cd
maxarvid <max.arvid.anderson@gmail.com>
2021-08-20 15:40:23 +0200

    add tests to see that creating a contact shows up on app

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -7,5 +7,14 @@ describe("user can create a contact", () => {
     cy.get("#phone").type("0705555555");
     cy.get("#company").type("free agent");
     cy.get("#twitter").type("@maxarvid");
+    cy.get("#submit").click();
+  });
+
+  it("displays the name of the new contact", () => {
+    cy.get("#contact-list").should("contain", "Max");
+  });
+
+  it("displays the phone number of the new contact", () => {
+    cy.get("#contact-list").should("contain", "0705555555");
   });
 });
