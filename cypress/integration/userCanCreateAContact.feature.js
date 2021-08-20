dee78216e2a2ce2cdfe37c2117dc161b6d77ab8e
maxarvid <max.arvid.anderson@gmail.com>
2021-08-20 15:27:42 +0200

    add form fill test and html implementation

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,6 +1,11 @@
 describe("user can create a contact", () => {
   it("test", () => {
     cy.visit("http://localhost:3001");
-    cy.get('#add-contact').click();
+    cy.get("#add-contact").click();
+    cy.get("#name").type("Max");
+    cy.get("#email").type("max@max.max");
+    cy.get("#phone").type("0705555555");
+    cy.get("#company").type("free agent");
+    cy.get("#twitter").type("@maxarvid");
   });
 });
